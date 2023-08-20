23312a6572fa6f2f7398ed770e058cfa645b60ac
gustcoder <gustavofaramos@gmail.com>
2023-08-20 14:53:04 -0300

    (6. Primeiro Projeto em Python) - Módulos
    - Validando argumento numérico
    - Exemplo de estrutura de condição

--- fundamentos_projetos/area_circulo_v14.py
+++ fundamentos_projetos/area_circulo_v14.py
@@ -20,10 +20,11 @@ if __name__ == "__main__":
         show_help("É necessário informar {} <raio>", sys.argv[0])
         # sys.exit(0) - terminou com sucesso
         # sys.exit(1) - terminou com erro, que é o mesmo que EPERM
-        sys.exit(errno.EPERM) # importando errno podemos passar o tipo de erro especifico
+        sys.exit(errno.EPERM)  # importando errno podemos passar o tipo de erro especifico
         print("Já não passa mais aqui!")
     elif not sys.argv[1].isnumeric():
         show_help("<raio> precisa ser numérico!")
+        sys.exit(errno.EINVAL)  # erro de argumento invalido
     else:
         raioStr = sys.argv[1]  # recebe parametro direto via comando - executar nome do arquivo + valor do raio
         area_circulo = circle_area(float(raioStr))
