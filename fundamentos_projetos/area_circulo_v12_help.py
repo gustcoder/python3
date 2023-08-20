cea9bf91bbb8aa03779f6d0d1a3c7d4029811743
gustcoder <gustavofaramos@gmail.com>
2023-08-20 14:36:04 -0300

    (6. Primeiro Projeto em Python) - Módulos
    - Melhorando o help

---
+++ fundamentos_projetos/area_circulo_v12_help.py
@@ -0,0 +1,24 @@
+"""
+Calculating circle area
+"""
+# import math
+from math import pi
+import sys
+
+
+def show_help(texto, parametro):
+    print(texto.format(parametro))
+
+
+def circle_area(raio):
+    return pi * (raio ** 2)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        show_help("É necessário informar {} <raio>", sys.argv[0])
+    else:
+        raioStr = sys.argv[1]  # recebe parametro direto via comando - executar nome do arquivo + valor do raio
+        area_circulo = circle_area(float(raioStr))
+
+        print(f'A área do círculo é: {area_circulo}')
